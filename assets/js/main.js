6820540f70933cdfb171e18e9aadf34796bf38f5
OmarAliSoliman <49602133+OmarAliSoliman@users.noreply.github.com>
2020-06-03 20:58:14 +0200

    first-commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
 
   "use strict";
 
+  AOS.init();
+
   var navBar = $('.uppernav .navbar');
   $(window).scroll(()=>{
     if($(this).scrollTop() > 10){
@@ -12,5 +14,43 @@ $(document).ready(function(){
   })
 
 
+  $('.app-slider').slick({
+    arrows: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots:false,
+    // centerMode: true,
+    responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        // centerMode: true,
 
+      }
+
+    }, {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots: false,
+        infinite: true,
+
+      }
+    },  {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      }
+    }]
+  });
 })
